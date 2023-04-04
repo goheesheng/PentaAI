1a9753380b86001c678f634c80ad453ee6ca7250
Goh Ee Sheng <goheesheng@hotmail.com>
2023-04-04 16:26:49 +0800

    Use shapes.h5 to predict

---
+++ Flask/model.py
@@ -0,0 +1,28 @@
+from tensorflow.keras.models import load_model
+import numpy as np
+from tensorflow.keras.preprocessing import image
+
+
+model = load_model(r"Flask\shapes.h5") # loading our model
+
+def predict(InputImg):
+    
+    img = image.load_img(InputImg, color_mode='grayscale',target_size = (64,64))
+    x = image.img_to_array(img) #converting image to array
+    x = np.expand_dims(x, axis=0)  # Add an extra dimension to the input data
+    prediction = model.predict(x)
+    predicted_class = np.argmax(prediction)
+    index= {0: 'circle',
+        1: 'parallelogram',
+        2: 'rectangle',
+        3: 'rhombus',
+        4: 'square',
+        5: 'trapezoid',
+        6: 'triangle'}
+    print(f"Key: {predicted_class}")
+    result=str(index[predicted_class])
+    return result
+
+if __name__ == "__main__":
+    result = predict(r"Dataset\dataset\\test\\trapezoid\\trapezoid-2000.jpg")
+    print(result)
